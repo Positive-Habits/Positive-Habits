56be8d0a1a2211aa9a3626c29d63feaa7aa71fee
coolelites <56234676+coolelites@users.noreply.github.com>
2019-11-22 00:54:12 -0600

    Add files via upload

--- All files/retrieve_data.js
+++ All files/retrieve_data.js
@@ -1,1 +1,5 @@
 document.getElementById('results').innerHTML = localStorage.getItem('goal1');
+document.getElementById('results2').innerHTML = localStorage.getItem('start');
+document.getElementById('results3').innerHTML = localStorage.getItem('end');
+document.getElementById('results4').innerHTML = localStorage.getItem('Duration');
+document.getElementById('results5').innerHTML = localStorage.getItem('dropdown');
--- All files/storage.js
+++ All files/storage.js
@@ -1,9 +1,20 @@
 //localStorage.setItem("lastname","smith")
 //document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-console.log('storage loaded')
+console.log('storage loaded');
 function submitForm() {
 	goal1 = document.getElementById('goal1').value;
 	localStorage.setItem('goal1', goal1);
-	console.log('item set')
-	
+	console.log('item set');
+	start = document.getElementById('start').value;
+	localStorage.setItem('start', start);
+	//console.log('item set')
+	end = document.getElementById('end').value;
+	localStorage.setItem('end', end);
+	//console.log('item set');
+	Duration = document.getElementById('Duration').value;
+	localStorage.setItem('Duration', Duration);
+	//console.log('item set')
+	dropdown = document.getElementById('dropdown').value;
+	localStorage.setItem('dropdown', dropdown);
+	//console.log('item set');
 }
