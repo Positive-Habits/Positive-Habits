0bc3d4b916c94bfd5d76f6bbf9ee22f68bfd0f4c
venkygoka <43710077+venkygoka@users.noreply.github.com>
2019-12-06 02:15:23 -0600

    Add files via upload

--- All files/login.js
+++ All files/login.js
@@ -1,14 +1,13 @@
-function read(form){
-if(form.uname.value == "rawr" && form.psw.value == "yeet")
-	{
-      window.open('today.html');	
-	}
-	else if(form.uname.value == "cool" && form.psw.value =="elite")
-	{
-		window.open('today.html');
-	}
-	else
-	{
-		alert("wrong");
-	}
+// Login javascript
+
+logIn();
+
+function logIn() {
+    // Create a new instance of the user class
+    var user = Parse.User
+        .logIn("myname", "mypass").then(function(user) {
+            console.log('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
+    }).catch(function(error){
+        console.log("Error: " + error.code + " " + error.message);
+    });
 }
--- All files/retrieve_data.js
+++ All files/retrieve_data.js
@@ -3,3 +3,13 @@ document.getElementById('results2').innerHTML = localStorage.getItem('start');
 document.getElementById('results3').innerHTML = localStorage.getItem('end');
 document.getElementById('results4').innerHTML = localStorage.getItem('Duration');
 document.getElementById('results5').innerHTML = localStorage.getItem('dropdown');
+
+var counter = 0;
+
+function complete(){
+	counter++;
+	document.getElementById('results6').innerHTML = 'Yes';
+if (counter == 1){
+  alert("You have Earned the badge: Add 3 habits!");
+	}
+}
