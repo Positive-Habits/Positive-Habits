d07642372b726c8bf58223f384ec95a252c4c5c6
venkygoka <43710077+venkygoka@users.noreply.github.com>
2019-12-06 15:49:11 -0600

    Add files via upload

---
+++ All files/Habs-storage.js
@@ -0,0 +1,41 @@
+//localStorage.setItem("lastname","smith")
+//document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+
+function submitForm()
+{
+	goal1 = document.getElementById('goal1').value;
+	if (goal1 == "")
+	{
+    alert("Name must be filled out");
+		return false;
+  }
+	localStorage.setItem('goal1', goal1);
+
+	start = document.getElementById('start').value;
+	if (start == "")
+	{
+    alert("Start time must be filled out");
+		return false;
+  }
+	localStorage.setItem('start', start);
+
+	end = document.getElementById('end').value
+	if (end == "")
+	{
+    alert("End time must be filled out");
+		return false;
+  }
+	localStorage.setItem('end', end);
+
+	Duration = document.getElementById('Duration').value;
+	if (Duration == "")
+	{
+    alert("End time must be filled out");
+		return false;
+  }
+	localStorage.setItem('Duration', Duration);
+
+
+	dropdown = document.getElementById('dropdown').value;
+	localStorage.setItem('dropdown', dropdown);
+}
