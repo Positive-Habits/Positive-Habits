6b3f264ce98c0a5d3da34e32f15d55c28fc1fb2b
coolelites <56234676+coolelites@users.noreply.github.com>
2019-12-06 02:44:14 -0600

    Add files via upload

--- All files/login.js
+++ All files/login.js
@@ -1,13 +1,14 @@
-// Login javascript
-
-logIn();
-
-function logIn() {
-    // Create a new instance of the user class
-    var user = Parse.User
-        .logIn("myname", "mypass").then(function(user) {
-            console.log('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
-    }).catch(function(error){
-        console.log("Error: " + error.code + " " + error.message);
-    });
+function read(form){
+if(form.uname.value == "rawr" && form.psw.value == "yeet")
+	{
+      window.open('today.html');	
+	}
+	else if(form.uname.value == "cool" && form.psw.value =="elite")
+	{
+		window.open('today.html');
+	}
+	else
+	{
+		alert("wrong");
+	}
 }
