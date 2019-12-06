07286e09ad5429bfefc190842e070f25214164c3
coolelites <56234676+coolelites@users.noreply.github.com>
2019-12-06 01:55:54 -0600

    Add files via upload

---
+++ All files/login.js
@@ -0,0 +1,14 @@
+function read(form){
+if(form.uname.value == "rawr" && form.psw.value == "yeet")
+	{
+      window.open('today.html');	
+	}
+	else if(form.uname.value == "cool" && form.psw.value =="elite")
+	{
+		window.open('today.html');
+	}
+	else
+	{
+		alert("wrong");
+	}
+}
