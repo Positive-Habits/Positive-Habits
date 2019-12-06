8e466e114552d0f164d313c072b78af12e985a70
venkygoka <43710077+venkygoka@users.noreply.github.com>
2019-12-06 16:07:15 -0600

    Delete retrieve_reminders.js

--- All files/retrieve_reminders.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('results1').innerHTML = localStorage.getItem('hab');
-document.getElementById('results2').innerHTML = localStorage.getItem('retime');
-document.getElementById('results3').innerHTML = localStorage.getItem('prior');
-document.getElementById('results4').innerHTML = localStorage.getItem('note');
